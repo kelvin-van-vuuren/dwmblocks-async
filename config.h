6863b1f54adc5f9431a83c1b2e6c780742db5d84
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2022-11-25 10:43:37 +0000

    Date module

--- config.h
+++ config.h
@@ -2,7 +2,8 @@
 #define DELIMITER "   "
 #define CLICKABLE_BLOCKS
 #define STATUSSEP ";"
-#define STATUSSEPINDEX 6
+#define STATUSSEPINDEX 7
+// #define LEADING_DELIMITER 1
 
 const Block blocks[] = {
 	BLOCK("sb-pacpackages",    1800,    17),
@@ -10,8 +11,9 @@ const Block blocks[] = {
 	BLOCK("sb-rss",            1800,    19),
 	BLOCK("sb-battery",        5,       20),
 	BLOCK("sb-internet",       5,       21),
-	BLOCK("sb-date",           1,       22), /* position of extra status split */
-	BLOCK("sb-disk",           1800,    23),
-	BLOCK("sb-memory",         3,       24),
-	BLOCK("sb-cpu",            3,       25),
+	BLOCK("sb-date",           1,       22),
+	BLOCK("sb-clock",          1,       23), /* position of extra status split */
+	BLOCK("sb-disk",           1800,    24),
+	BLOCK("sb-memory",         3,       25),
+	BLOCK("sb-cpu",            3,       26),
 };
