53949e7f2410a1c8321e4138f2f1289bfb243c96
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2022-11-28 13:27:29 +0000

    Weather forecast block

--- config.h
+++ config.h
@@ -2,18 +2,19 @@
 #define DELIMITER "   "
 #define CLICKABLE_BLOCKS
 #define STATUSSEP ";"
-#define STATUSSEPINDEX 7
+#define STATUSSEPINDEX 8
 // #define LEADING_DELIMITER 1
 
 const Block blocks[] = {
-	BLOCK("sb-pacpackages",    1800,    17),
-	BLOCK("sb-mail",           1800,    18),
-	BLOCK("sb-rss",            1800,    19),
-	BLOCK("sb-battery",        5,       20),
-	BLOCK("sb-internet",       5,       21),
-	BLOCK("sb-date",           1,       22),
-	BLOCK("sb-clock",          1,       23), /* position of extra status split */
-	BLOCK("sb-disk",           1800,    24),
-	BLOCK("sb-memory",         3,       25),
-	BLOCK("sb-cpu",            3,       26),
+	BLOCK("sb-forecast",       18000,   17),
+	BLOCK("sb-pacpackages",    1800,    18),
+	BLOCK("sb-mail",           1800,    19),
+	BLOCK("sb-rss",            1800,    20),
+	BLOCK("sb-battery",        5,       21),
+	BLOCK("sb-internet",       5,       22),
+	BLOCK("sb-date",           1,       23),
+	BLOCK("sb-clock",          1,       24), /* position of extra status split */
+	BLOCK("sb-disk",           1800,    25),
+	BLOCK("sb-memory",         3,       26),
+	BLOCK("sb-cpu",            3,       27),
 };
