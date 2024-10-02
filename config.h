ecdc4ee3bdd96a55ede8852993d2e1931781a8f3
Kelvin van Vuuren <kelvin.vanvuuren@skyral.io>
2024-10-02 12:20:47 +0100

    for other laptop

--- config.h
+++ config.h
@@ -2,18 +2,20 @@
 #define DELIMITER "   "
 #define CLICKABLE_BLOCKS
 #define STATUSSEP ";"
-#define STATUSSEPINDEX 10
+#define STATUSSEPINDEX 7
 // #define LEADING_DELIMITER 1
 
 const Block blocks[] = {
-	BLOCK("sb-forecast-rain",  18000,   15),
-	BLOCK("sb-forecast-low",   18000,   16),
-	BLOCK("sb-forecast-high",  18000,   17),
-	BLOCK("sb-pacpackages",    0,       18),
-	BLOCK("sb-mail",           0,       19),
-	BLOCK("sb-rss",            0,       20),
+	// BLOCK("sb-forecast-rain",  18000,   15),
+	// BLOCK("sb-forecast-low",   18000,   16),
+	// BLOCK("sb-forecast-high",  18000,   17),
+	BLOCK("sb-aptpackages",    0,       18),
+	// BLOCK("sb-mail",           0,       19),
+	// BLOCK("sb-rss",            0,       20),
 	BLOCK("sb-battery",        5,       21),
 	BLOCK("sb-internet",       5,       22),
+	BLOCK("sb-volume",         5,       10),
+	BLOCK("sb-bluetooth",      5,       12),
 	BLOCK("sb-date",           1,       23),
 	BLOCK("sb-clock",          1,       24), /* position of extra status split */
 	BLOCK("sb-disk",           1800,    25),
